f37c32f3ae7b8c98f20d524b7f9a9557b5050687
Vladyslav112 <159327533+Vladyslav112@users.noreply.github.com>
2024-07-07 14:10:15 +0000

    Deploying to gh-pages from @ Vladyslav112/goit-js-hw-11@bc1459fe3dc2e4fe2dfa3ef58206cb9e62179584 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,14 @@
-import{S as l,i as u}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const f="44812074-4f691c7d06d5961145b89341f",d="https://pixabay.com/api/";function m(o){return fetch(`${d}?key=${f}&q=${encodeURIComponent(o)}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error("Failed to fetch images");return t.json()}).then(t=>t.hits)}let c=null;function h(o){const t=document.getElementById("gallery");t.innerHTML=o.map(n=>p(n)).join(""),c?c.refresh():c=new l(".gallery a")}function p({webformatURL:o,largeImageURL:t,tags:n,likes:s,views:e,comments:r,downloads:i}){return`
+import{i as l,S as d}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))c(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const s of o.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function n(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function c(t){if(t.ep)return;t.ep=!0;const o=n(t);fetch(t.href,o)}})();function u(e){const r=document.getElementById("gallery"),n=e.map(f).join("");r.innerHTML=n,h()}function f(e){return`
     <div class="photo-card">
-      <a href="${t}">
-        <img src="${o}" alt="${n}" loading="lazy" />
+      <a href="${e.largeImageURL}">
+        <img src="${e.webformatURL}" alt="${e.tags}" loading="lazy" />
       </a>
       <div class="info">
-        <p><b>Likes:</b> ${s}</p>
-        <p><b>Views:</b> ${e}</p>
-        <p><b>Comments:</b> ${r}</p>
-        <p><b>Downloads:</b> ${i}</p>
+        <p><b>Likes</b> ${e.likes}</p>
+        <p><b>Views</b> ${e.views}</p>
+        <p><b>Comments</b> ${e.comments}</p>
+        <p><b>Downloads</b> ${e.downloads}</p>
       </div>
     </div>
-  `}function a(o){u.error({title:"Error",message:o})}const g=document.getElementById("search-form"),y=document.getElementById("search-input");document.getElementById("gallery");g.addEventListener("submit",o=>{o.preventDefault();const t=y.value.trim();if(!t){a("Please enter a search term");return}m(t).then(n=>{n.length===0?a("Sorry, there are no images matching your search query. Please try again!"):h(n)}).catch(n=>{a("An error occurred while fetching images")})});
+  `}function i(e){l.error({title:"Error",message:e})}function m(){const e=document.createElement("div");e.className="loader",document.body.appendChild(e)}function a(){const e=document.querySelector(".loader");e&&e.remove()}function h(){new d(".photo-card a").refresh()}const p="44812074-4f691c7d06d5961145b89341f",y="https://pixabay.com/api/";function g(e){return fetch(`${y}?key=${p}&q=${encodeURIComponent(e)}&image_type=photo&orientation=horizontal&safesearch=true`).then(r=>{if(!r.ok)throw new Error("Failed to fetch images");return r.json()}).then(r=>r.hits)}const b=document.getElementById("search-form"),L=document.getElementById("search-input"),v=document.getElementById("gallery");b.addEventListener("submit",e=>{e.preventDefault();const r=L.value.trim();if(!r){i("Please enter a search term");return}v.innerHTML="",m(),g(r).then(n=>{a(),n.length===0?i("Sorry, there are no images matching your search query. Please try again!"):u(n)}).catch(n=>{a(),i("An error occurred while fetching images")})});
 //# sourceMappingURL=commonHelpers.js.map
